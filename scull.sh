93e5d87fa0caa3c2e3316c73f3954710d689d7de
likhith <likhith2018@gmail.com>
2024-12-11 17:23:19 +0530

    All files before error-checking sent

---
+++ scull.sh
@@ -0,0 +1,21 @@
+module="scull"
+device="scull"
+mode="664"
+# invoke insmod with all arguments we got
+# and use a pathname, as newer modutils don't look in . by default
+/sbin/insmod ./$module.ko $* || exit 1
+#removing stale nodes
+rm -f /dev/${device}[0-3] 
+
+major="50"
+#using static assignment
+mknod /dev/${device}0 c $major 0
+mknod /dev/${device}1 c $major 1
+mknod /dev/${device}2 c $major 2
+mknod /dev/${device}3 c $major 3
+# give appropriate group/permissions, and change the group.
+# Not all distributions have staff, some have "wheel" instead.
+group="staff"
+grep -q '^staff:' /etc/group || group="wheel"
+chgrp $group /dev/${device}[0-3]
+chmod $mode /dev/${device}[0-3]
